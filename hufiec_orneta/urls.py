81f2068fbb6e78caa80e5b9c6e6c9c4c54f2c1e0
piszu <piszu@piszu-asus.(none)>
2013-02-28 21:24:37 +0100

    Wersja 1.0.1

--- hufiec_orneta/urls.py
+++ hufiec_orneta/urls.py
@@ -5,28 +5,24 @@ from strona.models import Artykuly
 from django.contrib import admin
 admin.autodiscover()
 
-info_dict = {
-    'queryset': Artykuly.objects.filter(categories=1),
-}
 
 urlpatterns = patterns('',
-	url(r'^page/(?P<page>[0-9]+)/$', 'strona.views.artykuly_list', name='artykuly-list'),
-	url(r'^page/?$', 'strona.views.artykuly_list', name='artykuly-list'),
-	url(r'^news/(?P<slug>[\w\-_]+)/$', 'strona.views.artykuly_detail', name='artykuly-detail'),
-	url(r'^categories/(?P<slug>[\w\-_]+)/$', 'strona.views.category_news_list', name='category-news-list'),
-	url(r'^categories/(?P<slug>[\w\-_]+)/(?P<page>[0-9]+)/$', 'strona.views.category_news_list', name='category-news-list'),
-	url(r'^news/(?P<slug>[\w\-_]+)/$', 'strona.views.artykuly_detail', name='artykuly-detail'),
-
-
+	# user
+	url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+	url(r'^accounts/profile/$', 'django.contrib.auth.views.login'),
 	# panel administracyjny
 	url(r'^administrator/', include(admin.site.urls)),
-    url(r'^/?$', redirect_to, {'url': '/a/1/'}),
-
-
-	url(r'^a/(?P<number>[0-9]+)/$', 'strona.views.aktualnosci_view', name='aktualnosci_view'), # aktualnosci
-	url(r'^h/$', 'strona.views.hufiec_view', name='hufiec_view'), # hufiec
-	url(r'^h/(?P<number>[0-9]+)/$', 'strona.views.hufiec_view', name='hufiec_view'), # hufiec
-	url(r'^d/(?P<number>[0-9]+)/$', 'strona.views.druzyny_view', name='druzyny_view'), # druzyny
-	url(r'^r/(?P<number>[0-9]+)/$', 'strona.views.rodzice_view', name='rodzice_view'), # rodzice
+	#redirect na aktualności
+    url(r'^/?$', redirect_to, {'url': '/a/'}),
+	# aktualnosc
+	url(r'^a/$', 'strona.views.aktualnosci_first', name='aktualnosci_first'), 
+	url(r'^a/(?P<number>[0-9]+)/$', 'strona.views.aktualnosci_view', name='aktualnosci_view'), 
+	# hufiec
+	url(r'^h/$', 'strona.views.hufiec_first', name='hufiec_first'), 
+	url(r'^h/(?P<number>[0-9]+)/$', 'strona.views.hufiec_view', name='hufiec_view'), 
+	# druzyny
+	url(r'^d/(?P<number>[0-9]+)/$', 'strona.views.druzyny_view', name='druzyny_view'), 
+	# rodzice
+	url(r'^r/(?P<number>[0-9]+)/$', 'strona.views.rodzice_view', name='rodzice_view'), 
 )
 
